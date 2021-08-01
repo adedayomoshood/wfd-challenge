2910803ee47fa679f9b8fbe7f92399a5ad01d359
Adedayo Moshood <dharyour93@yahoo.com>
2021-08-01 19:02:07 +0100

    Implement Hero Transition

---
+++ plain-html/app/assets/scripts/main.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+  loopSlider();
+};
+
+var heroWrapper = document.querySelector('#heroSlider');
+var heroImages;
+
+if (heroWrapper) heroImages = heroWrapper.children;
+
+for (var i = 0; i < heroImages.length; i++) {
+  heroImages[i].classList.remove('none');
+}
+
+var currentImage = 0;
+var previousImage;
+
+function loopSlider() {
+  if (heroImages && heroImages.length > 0) {
+    heroImages[currentImage].setAttribute('class', 'active');
+
+    if (previousImage != null)
+      heroImages[previousImage].setAttribute('class', 'inactive');
+
+    previousImage = currentImage;
+    currentImage++;
+
+    if (currentImage === heroImages.length) currentImage = 0;
+
+    if (heroImages.length > 1) setTimeout(loopSlider, 4000);
+  }
+}
