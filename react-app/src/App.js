a3772e98faf44d20305e4d4daf5532d3e27e75e9
Adedayo Moshood <dharyour93@yahoo.com>
2021-08-02 07:24:42 +0100

    Add Hero to React App

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header.js';
-
+import Hero from './components/Hero.js';
 import './styles/app.scss';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Hero />
     </BrowserRouter>
   );
 }
---
+++ react-app/src/images/banners/index.js
@@ -0,0 +1,34 @@
+import Slider1Sm from './slide1-sm.jpg';
+import Slider1Md from './slide1-md.jpg';
+import Slider1Lg from './slide1-lg.jpg';
+
+import Slider2Sm from './slide2-sm.jpg';
+import Slider2Md from './slide2-md.jpg';
+import Slider2Lg from './slide2-lg.jpg';
+
+import Slider3Sm from './slide3-sm.jpg';
+import Slider3Md from './slide3-md.jpg';
+import Slider3Lg from './slide3-lg.jpg';
+
+import Slider4Sm from './slide4-sm.jpg';
+import Slider4Md from './slide4-md.jpg';
+import Slider4Lg from './slide4-lg.jpg';
+
+import Slider5Sm from './slide5-sm.jpg';
+import Slider5Md from './slide5-md.jpg';
+import Slider5Lg from './slide5-lg.jpg';
+
+import Slider6Sm from './slide6-sm.jpg';
+import Slider6Md from './slide6-md.jpg';
+import Slider6Lg from './slide6-lg.jpg';
+
+const banners = [
+  { sm: Slider1Sm, md: Slider1Md, lg: Slider1Lg },
+  { sm: Slider2Sm, md: Slider2Md, lg: Slider2Lg },
+  { sm: Slider3Sm, md: Slider3Md, lg: Slider3Lg },
+  { sm: Slider4Sm, md: Slider4Md, lg: Slider4Lg },
+  { sm: Slider5Sm, md: Slider5Md, lg: Slider5Lg },
+  { sm: Slider6Sm, md: Slider6Md, lg: Slider6Lg },
+];
+
+export default banners;
